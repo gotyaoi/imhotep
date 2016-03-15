8bd26d53e1dddf47128683e30d7847de80da92db
gotyaoi <gotyaoi@hotmail.com>
2016-03-15 16:16:03 -0700

    Slight cleanup to the first exercise.

--- maze/exercise/exercise1.py
+++ maze/exercise/exercise1.py
@@ -34,23 +34,24 @@ in escaping your current predicament.
 from .. import Maze, Walker, Win
 
 def main():
-    m = Maze(5)
-    m._maze = [[13, 14, 8, 10, 9],
-               [6, 9, 5, 12, 1],
-               [13, 5, 7, 5, 5],
-               [5, 5, 12, 1, 5],
-               [6, 2, 3, 7, 7]]
-    w = Walker(m)
-    w.power_on()
-    w._walker.screen.onkey(w._walker.getscreen().bye, 'q')
-    w._walker.screen.listen()
+    maze = Maze(5)
+    maze._maze = [[13, 14, 8, 10, 9],
+                  [6, 9, 5, 12, 1],
+                  [13, 5, 7, 5, 5],
+                  [5, 5, 12, 1, 5],
+                  [6, 2, 3, 7, 7]]
+    walker = Walker(maze)
+    screen = walker.getscreen()
+    walker.power_on()
+    screen.onkey(screen.bye, 'q')
+    screen.listen()
 
     from ..solution.exercise1 import instructions
 
     try:
-        w.run(instructions)
+        walker.run(instructions)
     except Win:
         print("Congratulations, you made it to the exit! Press q to quit.")
     else:
         print("You didn't make it to the exit. Press q to quit")
-    w._walker.screen.mainloop()
+    screen.mainloop()
