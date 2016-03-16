3093ecbd095711fe92cf636232caa6b5246ad7b5
gotyaoi <gotyaoi@hotmail.com>
2016-03-16 12:18:52 -0700

    Adding help and completion for the more dynamic menu items.

--- menu.py
+++ menu.py
@@ -1,38 +1,39 @@
+"""The interactive menu for the maze exercises."""
 from cmd import Cmd
 from importlib import import_module, reload
 from pathlib import Path
 
 
 class MyCmd(Cmd):
+    """Base class with some specialized exit commands."""
 
-    QUIT = set(('q', 'quit', 'exit', 'bye', 'EOF'))
+    QUIT = {'q', 'quit', 'exit', 'bye', 'EOF'}
 
     def emptyline(self):
         pass
 
-    def default(self, line):
-        cmd, _, line = self.parseline(line)
-        if cmd == 'EOF':
-            print()
-        if cmd in self.QUIT:
-            return True
-        super().default(line)
+for _quit in MyCmd.QUIT:
+    if _quit == 'EOF':
+        setattr(MyCmd, 'do_'+_quit, lambda self, arg: print() or True)
+    else:
+        setattr(MyCmd, 'do_'+_quit, lambda self, arg: True)
+    setattr(getattr(MyCmd, 'do_'+_quit), '__doc__',
+            'Go up one level, or quit if this is the top level.')
 
 class Outer(MyCmd):
+    """Menu listing all the exercises."""
 
-    EXERCISES = [x.stem for x in Path('maze/exercise').glob('*.py')]
+    EXERCISES = {x.stem for x in Path('maze/exercise').glob('*.py')}
 
     intro = 'Please choose an exercise. Available exercises:\n' + '\n'.join(EXERCISES)
     prompt = '> '
 
-    def onecmd(self, line):
-        cmd, _, line = self.parseline(line)
-        if cmd not in self.EXERCISES:
-            return super().onecmd(line)
-        inner = Inner(cmd)
-        inner.cmdloop()
+for _exercise in Outer.EXERCISES:
+    setattr(Outer, 'do_'+_exercise, lambda self, arg, ex=_exercise: Inner(ex).cmdloop())
+    setattr(getattr(Outer, 'do_'+_exercise), '__doc__', 'submenu for {}.'.format(_exercise))
 
 class Inner(MyCmd):
+    """Menu customizable for each exercise."""
 
     def __init__(self, exercise, *args, **kwargs):
         self.exercise = exercise
@@ -42,15 +43,17 @@ class Inner(MyCmd):
         self.prompt = exercise + '> '
         super().__init__(*args, **kwargs)
 
-    def do_info(self, arg):
+    def do_info(self, _):
+        """Print the story and other useful information for this exercise."""
         print(self.exercise_module.__doc__)
 
-    def do_reload(self, arg):
+    def do_reload(self, _):
+        """Reload the solution module. Run this after editing the module."""
         self.solution_module = reload(self.solution_module)
 
-    def do_run(self, arg):
+    def do_run(self, _):
+        """Run the exercise."""
         self.exercise_module.main()
 
 if __name__ == '__main__':
-    interp = Outer()
-    interp.cmdloop()
+    Outer().cmdloop()
