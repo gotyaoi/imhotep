0f79dad6524ac0fec5354e686b16849ad025db30
gotyaoi <gotyaoi@hotmail.com>
2016-03-18 14:45:30 -0700

    Cleaning up the menu and making it a bit more informative.

--- menu.py
+++ menu.py
@@ -7,25 +7,52 @@ from pathlib import Path
 class MyCmd(Cmd):
     """Base class with some specialized exit commands."""
 
-    QUIT = {'q', 'quit', 'exit', 'bye', 'EOF'}
+    QUIT = {'q', 'exit', 'bye', 'EOF'}
 
     def emptyline(self):
         pass
 
-for _quit in MyCmd.QUIT:
-    if _quit == 'EOF':
-        setattr(MyCmd, 'do_'+_quit, lambda self, arg: print() or True)
-    else:
-        setattr(MyCmd, 'do_'+_quit, lambda self, arg: True)
-    setattr(getattr(MyCmd, 'do_'+_quit), '__doc__',
-            'Go up one level, or quit if this is the top level.')
+    def default(self, line):
+        cmd, _, line = self.parseline(line)
+        if cmd in self.QUIT:
+            if cmd == 'EOF':
+                self.stdout.write('\n')
+            return True
+        return super().default(line)
+
+    def do_quit(self, _):
+        """Go up one menu, or quit if this is the top menu."""
+        return True
 
 class Outer(MyCmd):
     """Menu listing all the exercises."""
 
     EXERCISES = {x.stem for x in Path('maze/exercise').glob('*.py')}
 
-    intro = 'Please choose an exercise. Available exercises:\n' + '\n'.join(EXERCISES)
+    intro = '''
+     _.mmmmmmmmm._
+   dMMMY'~~~~~`YMMMb
+ dMMMY'         `YMMMb
+dMMMY'           `YMMMb
+CMMM(             )MMMD
+YMMMb.           .dMMMY
+ YMMMb.         .dMMMY
+  `YMMMboo...oodMMMY'
+.    `"#MMMMMMM#"'    .
+Mb       `MMM'       dM
+MMMM.   .dMMMb.   .dMMM
+MMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMM
+MMMM'   `YMMMY'   `YMMM
+MM'      )MMM(      `MM
+'       .MMMMM.       `
+        dMMMMMb
+       dMMMMMMMb
+        """""""
+Welcome to the Imhotep menu. Type in commands followed by the enter key.
+Use the command "help" to get a list of available commands, and "help <command>"
+to get help about a specific command. Use the command "quit" to exit the menu.
+Please choose an exercise. Available exercises:\n''' + '\n'.join(sorted(EXERCISES))
     prompt = '> '
 
 for _exercise in Outer.EXERCISES:
@@ -39,20 +66,20 @@ class Inner(MyCmd):
         self.exercise = exercise
         self.exercise_module = import_module('maze.exercise.' + exercise)
         self.solution_module = import_module('maze.solution.' + exercise)
-        self.intro = exercise + ':\ninfo\nreload\nrun\nquit'
+        self.intro = '''Available Commands:
+story - {}
+run - {}
+quit - {}'''.format(self.do_story.__doc__, self.do_run.__doc__, self.do_quit.__doc__)
         self.prompt = exercise + '> '
         super().__init__(*args, **kwargs)
 
-    def do_info(self, _):
+    def do_story(self, _):
         """Print the story and other useful information for this exercise."""
         print(self.exercise_module.__doc__)
 
-    def do_reload(self, _):
-        """Reload the solution module. Run this after editing the module."""
-        self.solution_module = reload(self.solution_module)
-
     def do_run(self, _):
         """Run the exercise."""
+        self.solution_module = reload(self.solution_module)
         self.exercise_module.main()
 
 if __name__ == '__main__':
