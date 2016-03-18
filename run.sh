fcc4938968da754be0dc80eca278409c73d02b85
Bennett Roesch <meta@spyrosoft.com>
2016-03-18 16:57:29 -0700

    Remove Python 3 tests, etc.

--- run.sh
+++ run.sh
@@ -3,11 +3,7 @@
 if hash python3 2>/dev/null; then
 	python3 menu.py
 elif hash python 2>/dev/null; then
-	if [[ `python -V` =~ "Python 3.4" || `python -V` =~ "Python 3.5" ]]; then
-		python menu.py
-	else
-		echo "Python 3.4 or up is required."
-	fi
+	python menu.py
 else
-	echo "Could not find an installed Python 3.4 or later in any of the following: $PATH"
+	echo "Could not find an installed version of Python in any of the following: $PATH"
 fi
