9c1b14b36b7c13cbbf013b98051f289899cfb080
gotyaoi <gotyaoi@hotmail.com>
2016-03-23 12:50:14 -0700

    Adding exercise7: the while statement.

---
+++ maze/exercise/exercise7.py
@@ -0,0 +1,20 @@
+"""A long corridor stretches out before you with no turns. The display of the
+machine, however, shows more of the maze just on the other side of the wall. It
+seems that the magic words are needed here, but where? Individually testing each
+inch of the wall seems pretty tedious...
+"""
+from .framework import exercise
+from random import randint
+
+def user_func(walker):
+    from ..solution.exercise7 import hand
+    hand(walker)
+
+def main():
+    maze = [[14, 10, 10, 10, 11],
+            [4, 10, 10, 10, 1],
+            [5, 13, 14, 10, 3],
+            [4, 2, 9, 12, 11],
+            [6, 11, 6, 2, 11]]
+    maze[0][randint(0, 4)] += 32
+    exercise(user_func, fields=maze)
