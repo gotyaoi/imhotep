e068e738613750214c8af91d9a5a4dbb85bb76a2
gotyaoi <gotyaoi@hotmail.com>
2016-03-23 14:04:50 -0700

    Adding exercise7: variables. Moving old exercise 7 to exercise8.

--- maze/exercise/exercise7.py
+++ maze/exercise/exercise7.py
@@ -1,20 +1,21 @@
-"""A long corridor stretches out before you with no turns. The display of the
-machine, however, shows more of the maze just on the other side of the wall. It
-seems that the magic words are needed here, but where? Individually testing each
-inch of the wall seems pretty tedious...
+"""As you lay on you back after the most recent drop, you hear a strange
+grinding sound. On the display, you can see an opening moving up and down a
+section of wall. As the machine starts up, the opening stops moving. A number
+flashes on the screen.
 """
 from .framework import exercise
 from random import randint
 
-def user_func(walker):
-    from ..solution.exercise7 import hand
-    hand(walker)
-
 def main():
     maze = [[14, 10, 10, 10, 11],
-            [4, 10, 10, 10, 1],
+            [12, 10, 10, 10, 9],
             [5, 13, 14, 10, 3],
             [4, 2, 9, 12, 11],
             [6, 11, 6, 2, 11]]
-    maze[0][randint(0, 4)] += 32
+    rand = randint(0, 4)
+    maze[0][rand] &= 13
+    maze[1][rand] &= 7
+    def user_func(walker):
+        from ..solution.exercise7 import hand
+        hand(walker, rand)
     exercise(user_func, fields=maze)
--- maze/solution/exercise7.py
+++ maze/solution/exercise7.py
@@ -1,32 +1,12 @@
 # In this exercise don't worry about this line for now.
-def hand(walker):
+def hand(walker, number):
     # In python being indented like this is called being inside a block. Make
     # sure anything you write in this exercise is indented by 4 spaces.
 
-    # In this exercise, one of the eastern walls of this corridor is openable,
-    # but which one it is is randomized each time you run the exercise. You
-    # could write a bunch of if statements to check each one, but there's a more
-    # concise way.
+    # In this exercise, one of the eastern walls of this corridor will be open,
+    # but which one it is is randomized each time you run the exercise. Luckily,
+    # we provide you a variable called number in this block which is how far
+    # forward you need to move to get to the opening.
 
-    # The while statement is sort of like the if statement, but with a twist.
-    # Like the if statement, the block inside the while statement is only run if
-    # the thing the while statement is checking is True. However, when the end
-    # of the while statement's block is reached, it goes back to the top and
-    # checks the condition again. If the condition is still True, it runs the
-    # block again. It does this until the condition is False.
-
-    # while somevariable:
-    #    do_stuff()
-
-    # Now, because the open_sesame method returns a True list when it opens a
-    # wall, this is sort of the opposite of what we want, to exit the while
-    # statement when a wall is opened. You can use "not" to reverse the True or
-    # False that an if or while is checking.
-
-    # while not somevariable:
-    #     do_stuff()
-
-    # Finally you may want to use a variable to keep track of how far up the
-    # corridor you've gone, so you can tell how far back to go. You can then use
-    # this variable in the instruction list you pass to run.
+    # Use the provided variable in the instruction list you pass to run.
     pass
