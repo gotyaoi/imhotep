2f05698e42aff3e952ca7a06a9c7df3729a9b393
gotyaoi <gotyaoi@hotmail.com>
2016-03-23 11:29:10 -0700

    Adding exercise5: objects.

---
+++ maze/exercise/exercise5.py
@@ -0,0 +1,27 @@
+"""As you recover from that last drop, the view outside the machine's windows
+draws your attentions. You're surrounded by walls in all four directions.
+Going out to examine the situation, you can see no way out. The walls are solid
+and well constructed, not a crack or seam to be found. You resign yourself to
+your eventual death from starvation.
+
+Suddenly, one of the walls slides down into the floor, revealing a passage.
+There's a shout of surprise from inside the machine. "My friend, come see this!"
+Inside, Fulan is resting his hand on a metal panel, above which are some
+characters you can't quite make out. "This panel was revealed after the last
+drop." he says. "The writing above it says 'Say the magic words to pass.', so I
+tried a few. It turned out to be 'Open Sesame', of all things." He shakes his
+head. "These ruins are getting stranger and stranger."
+"""
+from .framework import exercise
+
+def user_func(walker):
+    from ..solution.exercise5 import hand
+    hand(walker)
+
+def main():
+    maze = [[12, 10, 10, 10, 9],
+            [4, 8, 10, 9, 5],
+            [7, 4, 11, 5, 5],
+            [12, 0, 11, 7, 7],
+            [7, 6, 10, 27, 15]]
+    exercise(user_func, fields=maze)
---
+++ maze/solution/exercise5.py
@@ -0,0 +1,21 @@
+# In this exercise don't worry about this line for now.
+def hand(walker):
+    # In python being indented like this is called being inside a block. Make
+    # sure anything you write in this exercise is indented by 4 spaces.
+
+    # Inside this block, instead of having the run function, you have and object
+    # called walker, representing the maze walking machine. An object is simply
+    # a container which holds both data, and function that operate on that data.
+    # A function that lives in an object is called a method of that object. The
+    # run function from last exercise was actually a method of the walker
+    # object. To call a method of an object, you can write
+    # "objectname.methodname()".
+
+    # Aside from the run method, the walker object also has an open_sesame
+    # method. This method takes no arguments, and will open certain walls in the
+    # maze when the machine is next to them. For this maze, the wall to the
+    # south of the exit can be opened.
+
+    # pass simply means that there's nothing here at the moment. Delete the pass
+    # and replace it with your own code.
+    pass
