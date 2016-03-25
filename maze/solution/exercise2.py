ec6e0705cdf8744bf1be0cf98c7c8c1f9b4d876d
gotyaoi <gotyaoi@hotmail.com>
2016-03-24 17:22:00 -0700

    Clarifying mixing ints and strings in the instruction list.
    Fixes #15

--- maze/solution/exercise2.py
+++ maze/solution/exercise2.py
@@ -1,10 +1,16 @@
-# You can now use numbers in the instruction list to repeat the following
-# instruction the specified number of times.
-
 # In python, a number on it's own like 3, is called an int, short for integer.
 # ints can represent any non-fractional, rational number, including negative
-# numbers, positive numbers and zero. Make sure you don't quote the numbers in
-# the instruction list! If you did, they would be strings, not numbers.
+# numbers, positive numbers and zero.
 
-# Don't forget that all elements of a list must be separated by commas.
+# You can now use ints in the instruction list to repeat the following
+# instruction the specified number of times. The int is an element of the list
+# the same as the strings you've been using so far. For example, a list that
+# previously looked like this:
+# ['west', 'west', 'west', 'west']
+# Could now look like this:
+# [4, 'west']
+
+# Make sure you don't quote the numbers in the instruction list! If you did,
+# they would be strings, not numbers. Don't forget that all elements of a list
+# must be separated by commas.
 instructions = []
